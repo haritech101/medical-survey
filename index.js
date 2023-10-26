05e098d54ef5b49540f2e47cf23ecb67cfa5653f
Yogesh Jadhav <yjadhav@gmail.com>
2023-10-26 17:22:48 +0530

    First commit by Yogesh, added start script and added fields and validations to the survey form

--- index.js
+++ index.js
@@ -1,15 +1,74 @@
 let jsonForSurvey = {
     elements: [
+        
         {
             name: "lr",
             title: "LR",
             type: "text",
+            "isRequired": true,
+            "validators": [
+                { "type": "numeric", "text": "Value must be a number" }
+            ]
         },
         {
             name: "wdr",
             title: "WDR",
-            type: "text",
+            type: "radiogroup",
+            "isRequired": true,
+            "requiredErrorText": "Value cannot be empty",
+            choices: [
+                "Yes",
+                "No"
+            ]
+        },        
+        { 
+            type: "radiogroup", 
+            name: "price to competitors",
+            title: "Compared to our competitors, do you feel the Product is",
+            "isRequired": true,
+            "requiredErrorText": "Value cannot be empty",
+            choices: [
+                "Less expensive", 
+                "Priced about the same", 
+                "More expensive", 
+                "Not sure"
+            ]
         },
+        { 
+            type: "radiogroup",                 
+            name: "price", 
+            title: "Do you feel our current price is merited by our product?",
+            "isRequired": true,
+            "requiredErrorText": "Value cannot be empty",
+                
+            choices: [
+                "correct|Yes, the price is about right",
+                "low|No, the price is too low for your product",
+                "high|No, the price is too high for your product"
+            ]
+        },
+            
+        { 
+            type: "multipletext", 
+            name: "pricelimit", 
+            title: "What is the... ",
+                
+            items: [
+                { 
+                    name: "mostamount", 
+                    title: "Most amount you would every pay for a product like ours" 
+                },
+                    
+                { 
+                    name: "leastamount", 
+                    title: "The least amount you would feel comfortable paying" 
+                }
+            ]
+        },
+
+                    
+            
+        
     ],
 };
 
