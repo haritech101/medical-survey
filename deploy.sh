30c33ba94b5c934e12ccc8a4c6e0dd05fa9d6361
Harikrishna Natrajan <hari@tech101.in>
2023-11-04 07:54:48 +0530

    Ask target server to pull latest images before running the containers

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ TARGET_PATH="/home/dev/docker/medical-survey"
 
 ssh $TARGET_MACHINE mkdir -p $TARGET_PATH
 scp docker-compose.yml ${TARGET_MACHINE}:${TARGET_PATH}/
+ssh ${TARGET_MACHINE} docker-compose -f ${TARGET_PATH}/docker-compose.yml pull
 ssh $TARGET_MACHINE docker-compose -f ${TARGET_PATH}/docker-compose.yml up -d
